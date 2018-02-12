434a4bf1733a2e8525aa4d2db796300e80b3fbdd
RichardQin918 <richardqin918@gmail.com>
2018-02-12 12:09:42 -0500

    try out banner image not showing

--- js/layout.js
+++ js/layout.js
@@ -77,7 +77,7 @@ var Layout = function () {
         var canvasDiv = document.getElementById('particle-canvas');
         var options = {
           particleColor: '#888',
-          background: '../img/bgd.jpg',
+          background: '../img/background1.jpg',
           interactive: true,
           speed: 'medium',
           density: 'high'

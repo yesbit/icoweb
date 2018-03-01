ff536b986423bb354d28a326fba033251575d16f
RichardQin918 <richardqin918@gmail.com>
2018-03-01 16:40:00 -0500

    commit Mar 1st follow up with video frame edit

--- js/layout.js
+++ js/layout.js
@@ -148,7 +148,6 @@ var Layout = function () {
         });
     }
 
-
     var handleLine = function(){
         function generateChartData() {
             var chartData = [];
@@ -234,6 +233,43 @@ var Layout = function () {
         });
     }
 
+    var handleVideo = function(){
+        var videoPlayButton, videoWrapper, videos, i;
+        videoWrapper = document.getElementsByClassName('video-wrapper');
+        videos = document.getElementsByClassName('videos');
+        for (var i=0; i < videos.length; i++) {
+            var video = videos[i];
+            console.log(videoWrapper);
+            var videoMethods = {
+                renderVideoPlayButton: function() {
+                    if (videoWrapper[i].contains(video)) {
+                        this.formatVideoPlayButton();
+                        video.classList.add('has-media-controls-hidden');
+                        videoPlayButton = document.getElementsByClassName('video-overlay-play-button')[0];
+                        videoPlayButton.addEventListener('click', this.hideVideoPlayButton);
+                    }
+                },
+
+                formatVideoPlayButton: function() {
+                    videoWrapper[i].insertAdjacentHTML('beforeend', '\
+                        <svg class="video-overlay-play-button" viewBox="0 0 200 200" alt="Play video">\
+                            <circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#fff"/>\
+                            <polygon points="70, 55 70, 145 145, 100" fill="#fff"/>\
+                        </svg>\
+                    ');
+                },
+
+                hideVideoPlayButton: function() {
+                    video.play();
+                    videoPlayButton.classList.add('is-hidden');
+                    video.classList.remove('has-media-controls-hidden');
+                    video.setAttribute('controls', 'controls');
+                }
+            };
+            videoMethods.renderVideoPlayButton();
+        }
+    }
+
     return {
         init: function () {
             handleHeaderOnScroll(); // initial setup for fixed header
@@ -243,6 +279,7 @@ var Layout = function () {
             handlePie(); //piechart initialisation
             handleLine();
             handleSubscribe();
+            // handleVideo();
             // handle minimized header on page scroll
             $(window).scroll(function() {
                 handleHeaderOnScroll();
